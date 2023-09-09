a118a09efb7f7b477a57376d803fe22fadcfe23d
ReturnZeroGirl <121702377+ReturnZeroGirl@users.noreply.github.com>
2023-09-09 20:31:18 +0800

    Add files via upload

---
+++ GUI_CSharp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+
+public static void PrintMemoryUsageInMB()
+{
+    PerformanceCounter totalMemoryCounter = new PerformanceCounter("Memory", "Available MBytes");
+    PerformanceCounter usedMemoryCounter = new PerformanceCounter("Process", "Working Set", "_Total");
+
+    double totalMemoryInMB = totalMemoryCounter.NextValue();
+    double usedMemoryInMB = usedMemoryCounter.NextValue() / (1024.0 * 1024.0);
+
+    Console.WriteLine($"Total memory: {totalMemoryInMB} MB");
+    Console.WriteLine($"Used memory: {usedMemoryInMB} MB");
+}
---
+++ get.py
@@ -0,0 +1,17 @@
+import subprocess
+import re
+import re
+output = subprocess.check_output("systeminfo", shell=True, universal_newlines=True)
+physical_memory_total_match = re.search(r"物理内存总量:\s+([\d,]+) MB", output)
+physical_memory_available_match = re.search(r"可用的物理内存:\s+([\d,]+) MB", output)
+virtual_memory_total_match = re.search(r"虚拟内存: 最大值:\s+([\d,]+) MB", output)
+virtual_memory_available_match = re.search(r"虚拟内存: 使用中:\s+([\d,]+) MB", output)
+physical_memory_total = str(int(physical_memory_total_match.group(1).replace(",", "")))+"\n"
+physical_memory_available = str(int(physical_memory_available_match.group(1).replace(",", "")))+"\n"
+virtual_memory_total = str(int(virtual_memory_total_match.group(1).replace(",", "")))+"\n"
+virtual_memory_available = str(int(virtual_memory_available_match.group(1).replace(",", "")))+"\n"
+with open("get.txt","w+") as f:
+    f.write(physical_memory_total)
+    f.write(physical_memory_available)
+    f.write(virtual_memory_total)
+    f.write(virtual_memory_available)
